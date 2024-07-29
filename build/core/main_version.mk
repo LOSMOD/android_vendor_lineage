35ea1442e07dc8675a65cd5596755a32c214d888
RobertGarciaa <chae0218@naver.com>
2024-07-29 09:30:27 +0900

    mod

--- build/core/main_version.mk
+++ build/core/main_version.mk
@@ -18,8 +18,8 @@ ADDITIONAL_SYSTEM_PROPERTIES += \
 
 # LineageOS Platform SDK Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
+    ro.custom.build.version.plat.sdk=$(LINEAGE_PLATFORM_SDK_VERSION)
 
 # LineageOS Platform Internal Version
 ADDITIONAL_SYSTEM_PROPERTIES += \
-    ro.lineage.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
+    ro.custom.build.version.plat.rev=$(LINEAGE_PLATFORM_REV)
